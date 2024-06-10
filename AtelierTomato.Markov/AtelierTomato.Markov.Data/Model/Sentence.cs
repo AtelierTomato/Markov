7a68f4e5db83991839e5151e1f23e42163e96fa0
suflors <iammagical.space@gmail.com>
2024-06-10 16:17:54 -0700

    added Date to Sentence

--- AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/Sentence.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/Sentence.cs
@@ -1,6 +1,6 @@
 ﻿namespace AtelierTomato.Markov.Data.Model
 {
 	public record Sentence(
-		Guid ID, ObjectOID OID, AuthorOID Author, string Text
+		Guid ID, ObjectOID OID, AuthorOID Author, DateTimeOffset Date, string Text
 	);
 }

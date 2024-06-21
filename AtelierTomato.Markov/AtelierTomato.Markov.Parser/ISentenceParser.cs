f186361f7ba1c32bae4c14bbc281fd89abca565b
suflors <iammagical.space@gmail.com>
2024-06-20 19:55:51 -0700

    Added project AtelierTomato.Markov.Parser and interface ISentenceParser

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser/ISentenceParser.cs
@@ -0,0 +1,10 @@
+﻿namespace AtelierTomato.Markov.Parser
+{
+	public interface ISentenceParser
+	{
+		public IEnumerable<string> TokenizeText(string text);
+		public IEnumerable<string> ParseIntoSentenceTexts(string text);
+		public string ProcessText(string text);
+
+	}
+}

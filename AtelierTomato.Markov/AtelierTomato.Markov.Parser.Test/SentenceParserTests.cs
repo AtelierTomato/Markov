a0552183cd261e9e3ce7144d001c7c8d7e3aa640
suflors <iammagical.space@gmail.com>
2024-06-21 18:11:20 -0700

    added SentenceParserTests

--- AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/SentenceParserTests.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/SentenceParserTests.cs
@@ -1,11 +1,26 @@
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+
 namespace AtelierTomato.Markov.Parser.Test
 {
 	public class SentenceParserTests
 	{
-		[Fact]
-		public void Test1()
+		[Theory]
+		[InlineData(@"hello world how are you", @"hello world how are you")]
+		[InlineData(@"I\'m fine   and how are you", @"I \'m fine and how are you")]
+		[InlineData(@"wal*mart anime idol group wow", @"wal\*mart anime idol group wow")]
+		[InlineData(@"you don\'t like cheese .. .. .. not sure if i trust you anymore ...", @"you don \'t like cheese ...... not sure if i trust you anymore ...")]
+		[InlineData(@"the students\' council decided you die today", @"the students \' council decided you die today")]
+		[InlineData(@"check out http://zombo.com the best website", @"check out the best website")]
+		[InlineData(@"funny money :laala\: time yo", @"funny money \:laala \: time yo")]
+		public void ParseSimpleText(string input, string output)
 		{
+			var options = new SentenceParserOptions();
+			var target = new SentenceParser(Options.Create(options));
 
+			var result = target.ParseIntoSentenceTexts(input);
+
+			result.Should().ContainSingle().And.Contain(output);
 		}
 	}
 }

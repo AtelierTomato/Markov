5a7b38b874ef7a6834c8ddc80235b824b3d80be6
suflors <iammagical.space@gmail.com>
2024-06-21 18:40:47 -0700

    edited ParseSimpleText because the prior data made no sense, 7/7 running

--- AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/SentenceParserTests.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/SentenceParserTests.cs
@@ -7,12 +7,12 @@ namespace AtelierTomato.Markov.Parser.Test
 	{
 		[Theory]
 		[InlineData(@"hello world how are you", @"hello world how are you")]
-		[InlineData(@"I\'m fine   and how are you", @"I \'m fine and how are you")]
+		[InlineData(@"I'm fine   and how are you", @"I \'m fine and how are you")]
 		[InlineData(@"wal*mart anime idol group wow", @"wal\*mart anime idol group wow")]
-		[InlineData(@"you don\'t like cheese .. .. .. not sure if i trust you anymore ...", @"you don \'t like cheese ...... not sure if i trust you anymore ...")]
-		[InlineData(@"the students\' council decided you die today", @"the students \' council decided you die today")]
+		[InlineData(@"you don't like cheese .. .. .. not sure if i trust you anymore ...", @"you don \'t like cheese ...... not sure if i trust you anymore ...")]
+		[InlineData(@"the students' council decided you die today", @"the students \' council decided you die today")]
 		[InlineData(@"check out http://zombo.com the best website", @"check out the best website")]
-		[InlineData(@"funny money :laala\: time yo", @"funny money \:laala \: time yo")]
+		[InlineData(@"funny money :laala: time yo", @"funny money \:laala \: time yo")]
 		public void ParseSimpleText(string input, string output)
 		{
 			var options = new SentenceParserOptions();

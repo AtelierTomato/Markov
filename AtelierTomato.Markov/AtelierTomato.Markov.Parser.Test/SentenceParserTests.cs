cc03e2cbf8d6d4e7224f84761dc70e1a7a545b81
suflors <iammagical.space@gmail.com>
2024-06-21 18:50:42 -0700

    added ParseMultiSentenceText() 1/1 passing

--- AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/SentenceParserTests.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/SentenceParserTests.cs
@@ -22,5 +22,48 @@ namespace AtelierTomato.Markov.Parser.Test
 
 			result.Should().ContainSingle().And.Contain(output);
 		}
+
+		[Fact]
+		public void ParseMultiSentenceText()
+		{
+			var options = new SentenceParserOptions();
+			var target = new SentenceParser(Options.Create(options));
+
+			var input = @"
+War. War never changes.
+The Romans waged war to gather slaves and wealth.
+Spain built an empire from its lust for gold and territory.
+Hitler shaped a battered Germany into an economic superpower.
+
+But war never changes.
+
+In the 21st century, war was still waged over the resources that could be acquired. Only this time, the spoils of war were also its weapons: Petroleum and Uranium. For these resources, China would invade Alaska, the US would annex Canada, and the European Commonwealth would dissolve into quarreling, bickering nation-states, bent on controlling the last remaining resources on Earth.
+
+In 2077, the storm of world war had come again. In two brief hours, most of the planet was reduced to cinders. And from the ashes of nuclear devastation, a new civilization would struggle to arise.
+
+A few were able to reach the relative safety of the large underground vaults. Your family was part of that group that entered Vault Thirteen. Imprisoned safely behind the large Vault door, under a mountain of stone, a generation has lived without knowledge of the outside world.
+
+Life in the Vault is about to change.";
+			var output = new string[]
+			{
+				@"The Romans waged war to gather slaves and wealth .",
+				@"Spain built an empire from its lust for gold and territory .",
+				@"Hitler shaped a battered Germany into an economic superpower .",
+				@"In the 21st century , war was still waged over the resources that could be acquired .",
+				@"Only this time , the spoils of war were also its weapons \: Petroleum and Uranium .",
+				@"For these resources , China would invade Alaska , the US would annex Canada , and the European Commonwealth would dissolve into quarreling , bickering nation- states , bent on controlling the last remaining resources on Earth .",
+				@"In 2077 , the storm of world war had come again .",
+				@"In two brief hours , most of the planet was reduced to cinders .",
+				@"And from the ashes of nuclear devastation , a new civilization would struggle to arise .",
+				@"A few were able to reach the relative safety of the large underground vaults .",
+				@"Your family was part of that group that entered Vault Thirteen .",
+				@"Imprisoned safely behind the large Vault door , under a mountain of stone , a generation has lived without knowledge of the outside world .",
+				@"Life in the Vault is about to change ."
+			};
+
+			var result = target.ParseIntoSentenceTexts(input);
+
+			result.Should().BeEquivalentTo(output);
+		}
 	}
 }

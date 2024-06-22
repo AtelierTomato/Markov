3c7533a4fe1777eb6bba9e0e787296332e9241b6
suflors <iammagical.space@gmail.com>
2024-06-22 04:58:44 -0700

    Changed Sentence formatting for parity with ObjectOID and AuthorOID

--- AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/Sentence.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/Sentence.cs
@@ -1,6 +1,7 @@
 ﻿namespace AtelierTomato.Markov.Data.Model
 {
-	public record Sentence(
+	public record Sentence
+	(
 		Guid ID, ObjectOID OID, AuthorOID Author, DateTimeOffset Date, string Text
 	);
 }

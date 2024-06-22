53161feed2fd41fbdae7a773858766b69b2a6758
suflors <iammagical.space@gmail.com>
2024-06-22 02:43:06 -0700

    removed unecessary User parameter in IMarkovFilter

--- AtelierTomato.Markov/AtelierTomato.Markov.Data/IMarkovFilter.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/IMarkovFilter.cs
@@ -9,7 +9,6 @@ namespace AtelierTomato.Markov.Data
 		string? Guild { get; set; }
 		string? Channel { get; set; }
 		string? Subchannel { get; set; }
-		string? User { get; set; }
 		string? Keyword { get; set; }
 	}
 }

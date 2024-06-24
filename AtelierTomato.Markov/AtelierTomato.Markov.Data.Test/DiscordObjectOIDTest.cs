ac9befe12505b3394b02edfec8fa65f087841786
suflors <iammagical.space@gmail.com>
2024-06-23 21:11:49 -0700

    Changed DiscordObjectOID.Fragment to DiscordObjectOID.Sentence to be more clear as to what it is. Added Tests for DiscordObjectOID For functions. 6/6 passing.

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data.Test/DiscordObjectOIDTest.cs
@@ -0,0 +1,45 @@
+﻿using AtelierTomato.Markov.Data.Model.ObjectOID;
+using FluentAssertions;
+
+namespace AtelierTomato.Markov.Data.Test
+{
+	public class DiscordObjectOIDTest
+	{
+		[Fact]
+		public void DiscordInstanceToStringTest()
+		{
+			DiscordObjectOID discordMessage = DiscordObjectOID.ForInstance("discord.com");
+			discordMessage.ToString().Should().Be("Discord:discord.com");
+		}
+		[Fact]
+		public void DiscordInstanceToStringEscapeTest()
+		{
+			DiscordObjectOID discordMessage = DiscordObjectOID.ForInstance("di^scord:com");
+			discordMessage.ToString().Should().Be("Discord:di^^scord^:com");
+		}
+		[Fact]
+		public void DiscordServerToStringTest()
+		{
+			DiscordObjectOID discordMessage = DiscordObjectOID.ForServer("discord.com", 1253189664655806606);
+			discordMessage.ToString().Should().Be("Discord:discord.com:1253189664655806606");
+		}
+		[Fact]
+		public void DiscordChannelToStringTest()
+		{
+			DiscordObjectOID discordMessage = DiscordObjectOID.ForChannel("discord.com", 1253189664655806606, 1253270827257036801);
+			discordMessage.ToString().Should().Be("Discord:discord.com:1253189664655806606:1253270827257036801");
+		}
+		[Fact]
+		public void DiscordMessageToStringTest()
+		{
+			DiscordObjectOID discordMessage = DiscordObjectOID.ForMessage("discord.com", 1253189664655806606, 1253270827257036801, 1254633446295207966);
+			discordMessage.ToString().Should().Be("Discord:discord.com:1253189664655806606:1253270827257036801:1254633446295207966");
+		}
+		[Fact]
+		public void DiscordSentenceToStringTest()
+		{
+			DiscordObjectOID discordMessage = DiscordObjectOID.ForMessage("discord.com", 1253189664655806606, 1253270827257036801, 1254633446295207966);
+			discordMessage.ToString().Should().Be("Discord:discord.com:1253189664655806606:1253270827257036801:1254633446295207966");
+		}
+	}
+}
--- AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/ObjectOID/DiscordObjectOID.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/ObjectOID/DiscordObjectOID.cs
@@ -7,14 +7,14 @@
 		public ulong? Server { get; set; }
 		public ulong? Channel { get; set; }
 		public ulong? Message { get; set; }
-		public int? Fragment { get; set; }
-		private DiscordObjectOID(string instance, ulong? server = null, ulong? channel = null, ulong? message = null, int? fragment = null)
+		public int? Sentence { get; set; }
+		private DiscordObjectOID(string instance, ulong? server = null, ulong? channel = null, ulong? message = null, int? sentence = null)
 		{
 			Instance = instance;
 			Server = server;
 			Channel = channel;
 			Message = message;
-			Fragment = fragment;
+			Sentence = sentence;
 		}
 		public static DiscordObjectOID ForInstance(string instance)
 			=> new(instance);
@@ -24,9 +24,21 @@
 			=> new(instance, server, channel);
 		public static DiscordObjectOID ForMessage(string instance, ulong server, ulong channel, ulong message)
 			=> new(instance, server, channel, message);
-		public static DiscordObjectOID ForFragment(string instance, ulong server, ulong channel, ulong message, int fragment)
-			=> new(instance, server, channel, message, fragment);
+		public static DiscordObjectOID ForSentence(string instance, ulong server, ulong channel, ulong message, int sentence)
+			=> new(instance, server, channel, message, sentence);
 		public static DiscordObjectOID Parse(string OID) => throw new NotImplementedException();
-		public override string ToString() => throw new NotImplementedException();
+		public override string ToString()
+		{
+			IEnumerable<string?> stringRange = [
+				Service.ToString(),
+				ObjectOIDEscapement.Escape(Instance),
+				Server?.ToString() ?? null,
+				Channel?.ToString() ?? null,
+				Message?.ToString() ?? null,
+				Sentence?.ToString()?? null
+			];
+			stringRange = stringRange.Where(s => s is not null);
+			return string.Join(':', stringRange);
+		}
 	}
 }

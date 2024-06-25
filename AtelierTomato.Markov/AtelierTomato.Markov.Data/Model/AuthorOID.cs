76ee43f04c9eecdaeabd111690eb30548ae6aae7
suflors <iammagical.space@gmail.com>
2024-06-24 17:33:39 -0700

    Made AuthorOID.Instance and AuthorOID.Author not nullable. Changed AuthorOID to a class. Add AuthorOID.ToString().

--- AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/AuthorOID.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/AuthorOID.cs
@@ -1,7 +1,16 @@
 ﻿namespace AtelierTomato.Markov.Data.Model
 {
-	public record AuthorOID
-	(
-		ServiceType Service, string? Instance, string? Author
-	);
+	public class AuthorOID
+	{
+		public ServiceType Service { get; set; }
+		public string Instance { get; set; }
+		public string Author { get; set; }
+		public AuthorOID(ServiceType service, string instance, string author)
+		{
+			Service = service;
+			Instance = instance;
+			Author = author;
+		}
+		public string ToString() => string.Join(':', [Service.ToString(), ObjectOIDEscapement.Escape(Instance), ObjectOIDEscapement.Escape(Author)]);
+	}
 }

9ac7f8b1e08ff55813ecebda77d90b9af5e9c3f4
suflors <iammagical.space@gmail.com>
2024-06-26 21:39:31 -0700

    Added MarkovChainTest(). 0/1 passing.

--- AtelierTomato.Markov/AtelierTomato.Markov.Generation.Test/MarkovTests.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Generation.Test/MarkovTests.cs
@@ -1,4 +1,7 @@
 using AtelierTomato.Markov.Data;
+using AtelierTomato.Markov.Data.Model;
+using AtelierTomato.Markov.Data.Model.ObjectOID;
+using AtelierTomato.Markov.Data.SentenceAccess;
 using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -34,6 +37,42 @@ namespace AtelierTomato.Markov.Generation.Test
 
 			};
 			await action.Should().ThrowAsync<Exception>().WithMessage("Couldn't query any messages.");
+		}
+
+		[Fact]
+		public void MarkovChainTest()
+		{
+			List<Sentence> sentenceRange = [
+				new(
+					DiscordObjectOID.Parse("Discord:discord.com:1:1:1:1:1:1"),
+					AuthorOID.Parse("Discord:discord.com:1"),
+					DateTimeOffset.Now,
+					"lol this sentence is so cool"
+				),
+				new(
+					DiscordObjectOID.Parse("Discord:discord.com:1:1:1:1:1:2"),
+					AuthorOID.Parse("Discord:discord.com:1"),
+					DateTimeOffset.Now,
+					"this is my new sentence"
+				),
+				new(
+					DiscordObjectOID.Parse("Discord:discord.com:1:1:1:1:1:3"),
+					AuthorOID.Parse("Discord:discord.com:1"),
+					DateTimeOffset.Now,
+					"is my computer on or not ?"
+				),
+				new(
+					DiscordObjectOID.Parse("Discord:discord.com:1:1:1:1:1:4"),
+					AuthorOID.Parse("Discord:discord.com:1"),
+					DateTimeOffset.Now,
+					"my head hurts so bad"
+				),
+			];
+			InMemorySentenceAccess sentenceAccess = new();
+			sentenceAccess.WriteSentenceRange(sentenceRange);
+			GenerateMarkovSentence generator = new(sentenceAccess, Options.Create(new MarkovGenerationOptions { }));
+			var result = generator.Generate(new SentenceFilter(null, null, null), "lol").Result;
+			result.Should().Be("lol this is my head");
 		}
 	}
 }

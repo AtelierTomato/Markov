f9d002fbb20654819579463cc2805ef260c717ac
aliceif <7098860+aliceif@users.noreply.github.com>
2024-06-29 09:24:03 +0200

    add a customized builder for OIDs and use it

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/Model/OIDBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AtelierTomato.Markov.Data.Model;
+
+public class OIDBuilder
+{
+	private readonly StringBuilder stringBuilder;
+
+	public OIDBuilder(ServiceType serviceType)
+	{
+		this.stringBuilder = new StringBuilder(serviceType.ToString());
+	}
+
+	public OIDBuilder Append(string field)
+	{
+		this.stringBuilder.Append(":").Append(OIDEscapement.Escape(field));
+		return this;
+	}
+
+	public string Build() => this.stringBuilder.ToString();
+}

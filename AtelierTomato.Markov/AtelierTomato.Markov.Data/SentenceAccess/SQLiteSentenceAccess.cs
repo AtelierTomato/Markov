945a61a6ce9b505f98d801d66394cd0388703a7f
suflors <iammagical.space@gmail.com>
2024-06-29 22:42:34 -0700

    Added SQLiteSentenceAccess extending ISentenceAccess.

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Data/SentenceAccess/SQLiteSentenceAccess.cs
@@ -0,0 +1,37 @@
+﻿using AtelierTomato.Markov.Data.Model;
+
+namespace AtelierTomato.Markov.Data.SentenceAccess
+{
+	public class SQLiteSentenceAccess : ISentenceAccess
+	{
+		public Task DeleteSentenceRange(SentenceFilter filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Sentence?> ReadNextRandomSentence(List<string> prevList, List<string> previousIDs, SentenceFilter filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Sentence?> ReadRandomSentence(SentenceFilter filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Sentence>?> ReadSentenceRange(SentenceFilter filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task WriteSentence(Sentence sentence)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task WriteSentenceRange(IEnumerable<Sentence> sentenceRange)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

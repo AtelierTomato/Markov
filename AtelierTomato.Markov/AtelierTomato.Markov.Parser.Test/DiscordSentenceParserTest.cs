f61a3456ef1e918ce9bdbaa5e783009e1110a7cb
suflors <iammagical.space@gmail.com>
2024-07-04 03:35:02 -0700

    Added DiscordSentenceParserTest and ParseEmojiTest(). 0/1 passing.

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser.Test/DiscordSentenceParserTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+
+namespace AtelierTomato.Markov.Parser.Test
+{
+	public class DiscordSentenceParserTest
+	{
+		[Fact]
+		public void ParseEmojiTest()
+		{
+			var options = new SentenceParserOptions();
+			var target = new DiscordSentenceParser(Options.Create(options));
+
+			var result = target.ParseIntoSentenceTexts(@"one two three four <:smimsy:1166315314548576306>");
+
+			result.Should().ContainSingle().And.Contain(@"one two three four e:smimsy:");
+		}
+	}
+}

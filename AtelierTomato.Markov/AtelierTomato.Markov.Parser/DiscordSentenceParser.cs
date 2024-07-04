e2fc164e7c4396ab005dc7323d1bb359f6d2334f
suflors <iammagical.space@gmail.com>
2024-07-04 02:52:23 -0700

    Added DiscordSentenceParser extending off of SentenceParser.

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser/DiscordSentenceParser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace AtelierTomato.Markov.Parser
+{
+	public class DiscordSentenceParser(IOptions<SentenceParserOptions> options) : SentenceParser(options)
+	{
+
+	}
+}

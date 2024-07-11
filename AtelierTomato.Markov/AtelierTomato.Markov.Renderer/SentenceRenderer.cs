4bb389d2ea1f9bd595aa1fadfbae41e75a644917
suflors <iammagical.space@gmail.com>
2024-07-11 12:41:36 -0700

    Removed unnecessary groups from renderEmojiRegex.

--- AtelierTomato.Markov/AtelierTomato.Markov.Renderer/SentenceRenderer.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Renderer/SentenceRenderer.cs
@@ -5,7 +5,7 @@ namespace AtelierTomato.Markov.Renderer
 {
 	public class SentenceRenderer
 	{
-		private readonly Regex renderEmojiRegex = new Regex(@"(e:)((?:(?!:).)+)(:)", RegexOptions.Compiled);
+		private readonly Regex renderEmojiRegex = new Regex(@"e:((?:(?!:).)+):", RegexOptions.Compiled);
 		private readonly Regex attachToPreviousWord = new Regex(@"(?: |^)([.\}\)\];:]|[?!,]+)(?: |$)", RegexOptions.Compiled);
 		private readonly Regex attachToNextWord = new Regex(@"(?: |^)([#\[\{\(])(?: |$)", RegexOptions.Compiled);
 
@@ -24,6 +24,6 @@ namespace AtelierTomato.Markov.Renderer
 			return text.Trim();
 		}
 
-		private string RenderEmojis(string text) => renderEmojiRegex.Replace(text, m => m.Groups[2].Value);
+		private string RenderEmojis(string text) => renderEmojiRegex.Replace(text, m => m.Groups[1].Value);
 	}
 }

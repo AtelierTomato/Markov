b383b27c22372e9d63f145b4da1afd854b2a2aa0
suflors <iammagical.space@gmail.com>
2024-07-11 12:25:10 -0700

    Added code for RenderEmojis(), 9/29 passing.

--- AtelierTomato.Markov/AtelierTomato.Markov.Renderer/SentenceRenderer.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Renderer/SentenceRenderer.cs
@@ -1,8 +1,11 @@
 ﻿
+using System.Text.RegularExpressions;
+
 namespace AtelierTomato.Markov.Renderer
 {
 	public class SentenceRenderer
 	{
+		private readonly Regex renderEmojiRegex = new Regex(@"(e:)((?:(?!:).)+)(:)", RegexOptions.Compiled);
 		public string Render(string text)
 		{
 			text = RenderDetachCharacters(text);
@@ -15,9 +18,6 @@ namespace AtelierTomato.Markov.Renderer
 			return text;
 		}
 
-		private string RenderEmojis(string text)
-		{
-			return text;
-		}
+		private string RenderEmojis(string text) => renderEmojiRegex.Replace(text, m => m.Groups[2].Value);
 	}
 }

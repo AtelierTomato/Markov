2a017efc3b63559e669ded9aa6bebcfc211f7c1a
suflors <iammagical.space@gmail.com>
2024-07-11 00:28:36 -0700

    Renamed all parameters "input" in DiscordSentenceParser to "text" for parity with SentenceParser.

--- AtelierTomato.Markov/AtelierTomato.Markov.Parser/DiscordSentenceParser.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Parser/DiscordSentenceParser.cs
@@ -7,48 +7,48 @@ namespace AtelierTomato.Markov.Parser
 	public class DiscordSentenceParser(IOptions<SentenceParserOptions> options) : SentenceParser(options)
 	{
 		private readonly Regex replaceEmojiPattern = new Regex(@"(<:)(.+)(:[0-9]+>)", RegexOptions.Compiled);
-		public IEnumerable<string> ParseIntoSentenceTexts(string input, IEnumerable<ITag> tags)
+		public IEnumerable<string> ParseIntoSentenceTexts(string text, IEnumerable<ITag> tags)
 		{
-			input = ReplaceTagEntities(input, tags);
-			return ParseIntoSentenceTexts(input);
+			text = ReplaceTagEntities(text, tags);
+			return ParseIntoSentenceTexts(text);
 		}
 
-		public override IEnumerable<string> ParseIntoSentenceTexts(string input)
+		public override IEnumerable<string> ParseIntoSentenceTexts(string text)
 		{
-			IEnumerable<string> result = base.ParseIntoSentenceTexts(input);
+			IEnumerable<string> result = base.ParseIntoSentenceTexts(text);
 			result = result.Select(ReplaceEmoji);
 			return result;
 		}
 
-		private string ReplaceEmoji(string input)
+		private string ReplaceEmoji(string text)
 		{
-			return replaceEmojiPattern.Replace(input, m => "e:" + m.Groups[2].Value + ":");
+			return replaceEmojiPattern.Replace(text, m => "e:" + m.Groups[2].Value + ":");
 		}
 
-		private static string ReplaceTagEntities(string input, IEnumerable<ITag> tags)
+		private static string ReplaceTagEntities(string text, IEnumerable<ITag> tags)
 		{
 			foreach (ITag tag in tags.OrderByDescending(e => e.Index))
 			{
 				if (tag.Value is null || tag.Value.ToString() is null)
 				{
-					input = input.Remove(tag.Index, tag.Length).Insert(tag.Index, "unknown");
+					text = text.Remove(tag.Index, tag.Length).Insert(tag.Index, "unknown");
 					continue;
 				}
 
 				switch (tag.Type)
 				{
 					case TagType.UserMention:
-						input = input.Remove(tag.Index, tag.Length).Insert(tag.Index, ((IUser)tag.Value).Username);
+						text = text.Remove(tag.Index, tag.Length).Insert(tag.Index, ((IUser)tag.Value).Username);
 						break;
 					case TagType.RoleMention:
 					case TagType.ChannelMention:
-						input = input.Remove(tag.Index, tag.Length).Insert(tag.Index, tag.Value.ToString()!);
+						text = text.Remove(tag.Index, tag.Length).Insert(tag.Index, tag.Value.ToString()!);
 						break;
 					case TagType.EveryoneMention:
-						input = input.Remove(tag.Index, tag.Length).Insert(tag.Index, "everyone");
+						text = text.Remove(tag.Index, tag.Length).Insert(tag.Index, "everyone");
 						break;
 					case TagType.HereMention:
-						input = input.Remove(tag.Index, tag.Length).Insert(tag.Index, "here");
+						text = text.Remove(tag.Index, tag.Length).Insert(tag.Index, "here");
 						break;
 					case TagType.Emoji:
 						// Ignore, emojis will be processed later using the raw text, the tags are unecessary. This is just here to mark that.
@@ -59,7 +59,7 @@ namespace AtelierTomato.Markov.Parser
 				}
 			}
 
-			return input;
+			return text;
 		}
 	}
 }

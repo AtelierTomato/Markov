d6eccf37b2d6aa6a81c6fb18ddbce3de35673878
suflors <iammagical.space@gmail.com>
2024-07-11 12:06:30 -0700

    Added RenderEmojis() and RenderDetachCharacters() that currently just return their input as placeholder.

--- AtelierTomato.Markov/AtelierTomato.Markov.Renderer/SentenceRenderer.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Renderer/SentenceRenderer.cs
@@ -1,8 +1,21 @@
-﻿namespace AtelierTomato.Markov.Renderer
+﻿
+namespace AtelierTomato.Markov.Renderer
 {
 	public class SentenceRenderer
 	{
 		public string Render(string text)
+		{
+			text = RenderDetachCharacters(text);
+			text = RenderEmojis(text);
+			return text;
+		}
+
+		private string RenderDetachCharacters(string text)
+		{
+			return text;
+		}
+
+		private string RenderEmojis(string text)
 		{
 			return text;
 		}

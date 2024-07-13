8129c44e9568d1d3dde776c07000a0b47407503e
suflors <iammagical.space@gmail.com>
2024-07-13 00:57:22 -0700

    Added Keyword class with Keyword.Find() utilizing KeywordOptions.

---
+++ AtelierTomato.Markov/AtelierTomato.Markov.Generation/Keyword.cs
@@ -0,0 +1,17 @@
+﻿using AtelierTomato.Markov.Data;
+using Microsoft.Extensions.Options;
+
+namespace AtelierTomato.Markov.Generation
+{
+	public class Keyword(IWordAccess wordAccess, IOptions<KeywordOptions> options)
+	{
+		private readonly IWordAccess wordAccess = wordAccess;
+		private readonly KeywordOptions options = options.Value;
+
+		public async Task<string> Find(string str)
+		{
+			var sentenceWords = (await wordAccess.ReadWordRange(str.Split(' '))).Where(x => x.Appearances > options.MinimumAppearancesForKeyword && options.IgnoreKeyword.Contains(x.Name));
+			return sentenceWords.MinBy(w => w.Appearances)?.Name ?? string.Empty;
+		}
+	}
+}

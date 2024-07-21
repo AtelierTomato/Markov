c4ff25d09b811164189144b6ce0fb44d39d0e36e
suflors <iammagical.space@gmail.com>
2024-07-21 05:50:52 -0700

    renamed Keyword to KeywordProvider and added a comment explaining what Find() does.

--- AtelierTomato.Markov/AtelierTomato.Markov.Generation/KeywordProvider.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Generation/KeywordProvider.cs
@@ -3,11 +3,16 @@ using Microsoft.Extensions.Options;
 
 namespace AtelierTomato.Markov.Generation
 {
-	public class Keyword(IWordStatisticAccess wordStatisticAccess, IOptions<KeywordOptions> options)
+	public class KeywordProvider(IWordStatisticAccess wordStatisticAccess, IOptions<KeywordOptions> options)
 	{
 		private readonly IWordStatisticAccess wordStatisticAccess = wordStatisticAccess;
 		private readonly KeywordOptions options = options.Value;
 
+		/// <summary>
+		/// Finds the keyword of <paramref name="str"/> by determining what the least common word in the string is.
+		/// </summary>
+		/// <param name="str">The string that will be used to determine the keyword.</param>
+		/// <returns></returns>
 		public async Task<string> Find(string str)
 		{
 			var sentenceWords = (await wordStatisticAccess.ReadWordStatisticRange(str.Split(' '))).Where(x => x.Appearances > options.MinimumAppearancesForKeyword && options.IgnoreKeyword.Contains(x.Name));
--- AtelierTomato.Markov/AtelierTomato.Markov.Generation/MarkovChainKeyword.cs
+++ AtelierTomato.Markov/AtelierTomato.Markov.Generation/MarkovChainKeyword.cs
@@ -3,16 +3,16 @@ using Microsoft.Extensions.Options;
 
 namespace AtelierTomato.Markov.Generation
 {
-	public class MarkovChainKeyword(ISentenceAccess sentenceAccess, IOptions<MarkovGenerationOptions> options, Keyword keyword) : MarkovChain(sentenceAccess, options)
+	public class MarkovChainKeyword(ISentenceAccess sentenceAccess, IOptions<MarkovGenerationOptions> options, KeywordProvider keywordProvider) : MarkovChain(sentenceAccess, options)
 	{
-		private readonly Keyword keyword = keyword;
+		private readonly KeywordProvider keywordProvider = keywordProvider;
 		public async Task<string> Generate(SentenceFilter filter, string str, string? firstWord)
 		{
 			if (filter.SearchString is not null)
 			{
 				throw new ArgumentException("You cannot generate a keyword if there is a keyword already present in the filter.", nameof(filter));
 			}
-			filter.SearchString = await keyword.Find(str);
+			filter.SearchString = await keywordProvider.Find(str);
 			return await base.Generate(filter, firstWord);
 		}
 	}

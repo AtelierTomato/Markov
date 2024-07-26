1d3c2af8dd953e8a9242b172558095b33a273208
suflors <iammagical.space@gmail.com>
2024-07-25 21:00:56 -0700

    Removed DiscordAuthorOIDBuilder as it is not necessary. Added DiscordSentenceBuilder.Build().

---
+++ src/AtelierTomato.Markov.Service.Discord/DiscordSentenceBuilder.cs
@@ -0,0 +1,23 @@
+﻿using AtelierTomato.Markov.Model;
+using AtelierTomato.Markov.Model.ObjectOID;
+using Discord.Commands;
+
+namespace AtelierTomato.Markov.Service.Discord
+{
+	public class DiscordSentenceBuilder
+	{
+		public static IEnumerable<Sentence> Build(IEnumerable<string> sentenceTexts, ICommandContext context, string instance = "discord.com")
+		{
+			DiscordObjectOID OID = DiscordObjectOIDBuilder.Build(context, instance);
+			AuthorOID Author = new(ServiceType.Discord, instance, context.User.Id.ToString());
+			return sentenceTexts.Select((text, index) =>
+				new Sentence(
+					DiscordObjectOID.ForSentence(OID.Instance, OID.Server!.Value, OID.Category!.Value, OID.Channel!.Value, OID.Thread!.Value, OID.Message!.Value, index),
+					Author,
+					context.Message.CreatedAt,
+					text
+				)
+			);
+		}
+	}
+}

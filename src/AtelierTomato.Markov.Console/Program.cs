42157ad334b206a98a47d1dde7d70c38a6f5c932
suflors <iammagical.space@gmail.com>
2024-08-07 17:06:14 -0700

    Added AtelierTomato.Markov.Service and a command to generate sentences.

---
+++ src/AtelierTomato.Markov.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using AtelierTomato.Markov.Console;
+using AtelierTomato.Markov.Core;
+using AtelierTomato.Markov.Core.Generation;
+using AtelierTomato.Markov.Storage;
+using AtelierTomato.Markov.Storage.Sqlite;
+
+IHost host = Host.CreateDefaultBuilder(args)
+	.UseSystemd()
+	.ConfigureAppConfiguration((hostContext, builder) =>
+	{
+		// Add other providers for JSON, etc.
+
+		// only use user secrets when debugging.
+		if (Debugger.IsAttached)
+		{
+			builder.AddUserSecrets<Program>();
+		}
+	})
+	.ConfigureLogging((hostContext, builder) =>
+	{
+		// if we're in fact using systemd, throw out the default console logger and only use the systemd journal
+		if (Microsoft.Extensions.Hosting.Systemd.SystemdHelpers.IsSystemdService())
+		{
+			builder.ClearProviders();
+			builder.AddJournal(options => options.SyslogIdentifier = hostContext.Configuration["SyslogIdentifier"]);
+		}
+	})
+	.ConfigureServices((hostContext, services) =>
+	{
+		services.AddHostedService<Worker>();
+
+		services.AddSingleton<ISentenceAccess, SqliteSentenceAccess>()
+				.AddSingleton<IWordStatisticAccess, SqliteWordStatisticAccess>()
+				.AddSingleton<MarkovChain>()
+				.AddSingleton<KeywordProvider>()
+				.AddSingleton<SentenceRenderer>();
+		services.AddOptions<SqliteAccessOptions>()
+				.Bind(hostContext.Configuration.GetSection("SqliteAccess"));
+		services.AddOptions<MarkovChainOptions>()
+				.Bind(hostContext.Configuration.GetSection("MarkovChain"));
+		services.AddOptions<KeywordOptions>()
+				.Bind(hostContext.Configuration.GetSection("Keyword"));
+	})
+	.Build();
+
+await host.RunAsync();
---
+++ src/AtelierTomato.Markov.Console/Worker.cs
@@ -0,0 +1,79 @@
+﻿
+using AtelierTomato.Markov.Core;
+using AtelierTomato.Markov.Core.Generation;
+using AtelierTomato.Markov.Model;
+
+namespace AtelierTomato.Markov.Console
+{
+	public class Worker : IHostedService
+	{
+		private readonly ILogger<Worker> logger;
+		private readonly MarkovChain markovChain;
+		private readonly SentenceRenderer sentenceRenderer;
+
+		public Worker(ILogger<Worker> logger, MarkovChain markovChain, SentenceRenderer sentenceRenderer)
+		{
+			this.logger = logger;
+			this.markovChain = markovChain;
+			this.sentenceRenderer = sentenceRenderer;
+		}
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			this.logger.LogInformation("hello");
+
+			_ = Looper(cancellationToken);
+		}
+
+		private async Task Looper(CancellationToken cancellationToken)
+		{
+			while (!cancellationToken.IsCancellationRequested)
+			{
+				System.Console.WriteLine("Enter a command name and press enter.");
+				System.Console.WriteLine("Valid commands: generate");
+				string? command = System.Console.ReadLine();
+				if (command is not null and "generate")
+				{
+					bool inputAccepted = false;
+					int sentencesToGenerate = 0;
+					while (!inputAccepted)
+					{
+						System.Console.WriteLine("Enter amount of sentences to generate, or press enter to use the default (10):");
+						string? input = System.Console.ReadLine();
+						if (input is null or "")
+						{
+							sentencesToGenerate = 10;
+							inputAccepted = true;
+						}
+						else if (int.TryParse(input, out sentencesToGenerate))
+						{
+							inputAccepted = true;
+						}
+						else
+						{
+							System.Console.WriteLine("Please enter an integer values.");
+						}
+					}
+					System.Console.WriteLine($"Generating {sentencesToGenerate} sentences...");
+					var tasks = new List<Task<string>>();
+					for (int i = 0; i < sentencesToGenerate; i++)
+					{
+						tasks.Add(markovChain.Generate(new SentenceFilter(null, null)));
+					}
+					List<string> sentences = (await Task.WhenAll(tasks)).Select(sentenceRenderer.Render).ToList();
+
+					for (int i = 0; i < sentences.Count; i++)
+					{
+						System.Console.WriteLine($"Sentence {i + 1}:");
+						System.Console.WriteLine(sentences[i]);
+					}
+				}
+			}
+		}
+
+		public async Task StopAsync(CancellationToken cancellationToken)
+		{
+			this.logger.LogInformation("goodbye");
+		}
+	}
+}

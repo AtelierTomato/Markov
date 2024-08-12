8ec5a4f01170b4ef33692ec296da9bd197902092
suflors <iammagical.space@gmail.com>
2024-08-11 23:40:31 -0700

    Split guild-less Build() into a different function.

--- src/AtelierTomato.Markov.Service.Discord/DiscordObjectOIDBuilder.cs
+++ src/AtelierTomato.Markov.Service.Discord/DiscordObjectOIDBuilder.cs
@@ -6,17 +6,18 @@ namespace AtelierTomato.Markov.Service.Discord
 	public class DiscordObjectOIDBuilder
 	{
 		/// <summary>
-		/// Builds a DiscordObjectOID at scope of <see cref="DiscordObjectOID.Channel"/> or <see cref="DiscordObjectOID.Thread"/> <paramref name="guild"/> and <paramref name="channel"/> are passed.
-		/// If <paramref name="messageID"/> is passed, builds at scope of <see cref="DiscordObjectOID.Message"/>.
+		/// Builds a DiscordObjectOID at scope of <see cref="DiscordObjectOID.Channel"/> or <see cref="DiscordObjectOID.Thread"/>.
 		/// </summary>
 		/// <param name="guild">The <see cref="IGuild"/> of the desired DiscordObjectOID. Must contain the <paramref name="channel"/>.</param>
-		/// <param name="channel">The <see cref="IChannel"/> of the desired DiscordObjectOID. Can be <see cref="IThreadChannel"/>. Must be inside the <paramref name="guild"/>.</param>
-		/// <param name="messageID">Optional parameter indicating <see cref="IMessage"/> ID, used if intending to build at scope <see cref="DiscordObjectOID.Message"/>.</param>
+		/// <param name="channel">The <see cref="IGuildChannel"/> of the desired DiscordObjectOID. Can be <see cref="IThreadChannel"/>. Must be inside the <paramref name="guild"/>.</param>
 		/// <param name="instance">Optional parameter, used if on an alternative instance of Discord.</param>
 		/// <returns></returns>
-		public static async Task<DiscordObjectOID> Build(IGuild? guild, IChannel channel, string instance = "discord.com")
+		public static async Task<DiscordObjectOID> Build(IGuild guild, IGuildChannel channel, string instance = "discord.com")
 		{
-			ulong serverID = guild?.Id ?? 0;
+			if (channel.GuildId != guild.Id)
+			{
+				throw new ArgumentException($"The {nameof(guild)} provided does not contain the {nameof(channel)} provided.", $"{nameof(guild)}, {nameof(channel)}");
+			}
 			ulong categoryID, channelID;
 			ulong? threadID;
 			if (channel is INestedChannel nestedChannel and not IThreadChannel)
@@ -27,7 +28,7 @@ namespace AtelierTomato.Markov.Service.Discord
 			}
 			else if (channel is IThreadChannel threadChannel)
 			{
-				if (guild is not null && await guild.GetChannelAsync(threadChannel.CategoryId!.Value) is not null and INestedChannel parentChannel && parentChannel.CategoryId is not null)
+				if (await guild.GetChannelAsync(threadChannel.CategoryId!.Value) is not null and INestedChannel parentChannel && parentChannel.CategoryId is not null)
 				{
 					categoryID = parentChannel.CategoryId.Value;
 				}
@@ -47,11 +48,11 @@ namespace AtelierTomato.Markov.Service.Discord
 
 			if (threadID is not null)
 			{
-				return DiscordObjectOID.ForThread(instance, serverID, categoryID, channelID, threadID.Value);
+				return DiscordObjectOID.ForThread(instance, guild.Id, categoryID, channelID, threadID.Value);
 			}
 			else
 			{
-				return DiscordObjectOID.ForChannel(instance, serverID, categoryID, channelID);
+				return DiscordObjectOID.ForChannel(instance, guild.Id, categoryID, channelID);
 			}
 		}
 	}
--- src/AtelierTomato.Markov.Service.Discord/DiscordSentenceBuilder.cs
+++ src/AtelierTomato.Markov.Service.Discord/DiscordSentenceBuilder.cs
@@ -7,9 +7,26 @@ namespace AtelierTomato.Markov.Service.Discord
 {
 	public class DiscordSentenceBuilder
 	{
-		public static async Task<IEnumerable<Sentence>> Build(IGuild? guild, IChannel channel, ulong messageID, ulong userID, DateTimeOffset date, IEnumerable<string> sentenceTexts, string instance = "discord.com")
+		public static async Task<IEnumerable<Sentence>> Build(IGuild guild, IGuildChannel channel, ulong messageID, ulong userID, DateTimeOffset date, IEnumerable<string> sentenceTexts, string instance = "discord.com")
 		{
 			DiscordObjectOID OID = (await DiscordObjectOIDBuilder.Build(guild, channel, instance)).WithMessage(messageID);
+			AuthorOID author = new(ServiceType.Discord, instance, userID.ToString(CultureInfo.InvariantCulture));
+			return sentenceTexts.Select((text, index) =>
+				new Sentence(
+					OID.WithSentence(index),
+					author,
+					date,
+					text
+				)
+			);
+		}
+		public static IEnumerable<Sentence> Build(IChannel channel, ulong messageID, ulong userID, DateTimeOffset date, IEnumerable<string> sentenceTexts, string instance = "discord.com")
+		{
+			if (channel is IGuildChannel)
+			{
+				throw new ArgumentException($"An {nameof(IGuildChannel)} was passed while this function is only used for non-guild channels. Please use the correct overload.", nameof(channel));
+			}
+			DiscordObjectOID OID = DiscordObjectOID.ForMessage(instance, 0, 0, channel.Id, 0, messageID);
 			AuthorOID author = new(ServiceType.Discord, instance, userID.ToString(CultureInfo.InvariantCulture));
 			return sentenceTexts.Select((text, index) =>
 				new Sentence(

f7bb5969f1596a9ec5ae4c7de64e4eadf8c4877d
suflors <iammagical.space@gmail.com>
2024-08-31 17:38:50 -0700

    Added interface IUserPermissionAccess.

---
+++ src/AtelierTomato.Markov.Storage/IUserPermissionAccess.cs
@@ -0,0 +1,13 @@
+﻿using AtelierTomato.Markov.Model;
+
+namespace AtelierTomato.Markov.Storage
+{
+	public interface IUserPermissionAccess
+	{
+		Task<IEnumerable<UserPermission>> ReadAllUserPermissions();
+		Task<UserPermission> ReadUserPermission(AuthorOID author, IObjectOID originScope);
+		Task<IEnumerable<UserPermission>> ReadUserPermissionRange(IEnumerable<AuthorOID> authors, IEnumerable<IObjectOID> originScopes);
+		Task WriteUserPermission(UserPermission userPermission);
+		Task WriteUserPermissionRange(IEnumerable<UserPermission> userPermissions);
+	}
+}

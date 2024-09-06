6d20575829a7bf0b3260108060491796e8c492a5
suflors <iammagical.space@gmail.com>
2024-09-05 22:38:53 -0700

    Added IAuthorGroupAccess and SqliteAuthorGroupAccess.

---
+++ src/AtelierTomato.Markov.Storage/IAuthorGroupAccess.cs
@@ -0,0 +1,11 @@
+﻿using AtelierTomato.Markov.Model;
+
+namespace AtelierTomato.Markov.Storage
+{
+	public interface IAuthorGroupAccess
+	{
+		Task<AuthorGroup?> ReadAuthorGroup(Guid ID);
+		Task WriteAuthorGroup(AuthorGroup authorGroup);
+		Task DeleteAuthorGroup(Guid ID);
+	}
+}

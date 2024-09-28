32f80a1dc55037c82b0d6a602b36d768c3736bce
suflors <iammagical.space@gmail.com>
2024-09-28 15:44:06 -0700

    Added LocationGroup, ILocationGroupAccess, and SqliteLocationGroupAccess.

---
+++ src/AtelierTomato.Markov.Model/LocationGroup.cs
@@ -0,0 +1,7 @@
+﻿namespace AtelierTomato.Markov.Model
+{
+	public record LocationGroup
+	(
+		Guid ID, string Name
+	);
+}
---
+++ src/AtelierTomato.Markov.Storage/ILocationGroupAccess.cs
@@ -0,0 +1,11 @@
+﻿using AtelierTomato.Markov.Model;
+
+namespace AtelierTomato.Markov.Storage
+{
+	public interface ILocationGroupAccess
+	{
+		Task<LocationGroup?> ReadLocationGroup(Guid ID);
+		Task WriteLocationGroup(LocationGroup locationGroup);
+		Task DeleteLocationGroup(Guid ID);
+	}
+}

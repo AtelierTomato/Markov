f1cabcfe46b9efb7e1567b0880c588c33520d8ea
suflors <iammagical.space@gmail.com>
2024-09-30 13:25:33 -0700

    Fixed AuthorPermission nullability.

--- src/AtelierTomato.Markov.Storage.Sqlite/Model/AuthorPermissionRow.cs
+++ src/AtelierTomato.Markov.Storage.Sqlite/Model/AuthorPermissionRow.cs
@@ -5,9 +5,9 @@ namespace AtelierTomato.Markov.Storage.Sqlite.Model
 	public class AuthorPermissionRow
 	{
 		public string Author { get; set; }
-		public string QueryScope { get; set; }
-		public string AllowedScope { get; set; }
-		public AuthorPermissionRow(string author, string queryScope, string allowedScope)
+		public string? QueryScope { get; set; }
+		public string? AllowedScope { get; set; }
+		public AuthorPermissionRow(string author, string? queryScope, string? allowedScope)
 		{
 			Author = author;
 			QueryScope = queryScope;
@@ -16,9 +16,20 @@ namespace AtelierTomato.Markov.Storage.Sqlite.Model
 		public AuthorPermissionRow(AuthorPermission authorPermission)
 		{
 			Author = authorPermission.Author.ToString();
-			QueryScope = authorPermission.QueryScope.ToString();
-			AllowedScope = authorPermission.AllowedScope.ToString();
+			QueryScope = authorPermission.QueryScope?.ToString();
+			AllowedScope = authorPermission.AllowedScope?.ToString();
 		}
-		public AuthorPermission ToAuthorPermission(MultiParser<IObjectOID> objectOIDParser) => new(AuthorOID.Parse(Author), objectOIDParser.Parse(QueryScope), objectOIDParser.Parse(AllowedScope));
+		public AuthorPermission ToAuthorPermission(MultiParser<IObjectOID> objectOIDParser)
+		{
+			return new(AuthorOID.Parse(Author), QueryScope switch
+			{
+				null => null,
+				_ => objectOIDParser.Parse(QueryScope),
+			}, AllowedScope switch
+			{
+				null => null,
+				_ => objectOIDParser.Parse(AllowedScope)
+			});
+		}
 	}
 }

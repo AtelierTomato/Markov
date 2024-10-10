4706507a3fdd51d322530aa910a9f69a5362d445
suflors <iammagical.space@gmail.com>
2024-10-09 20:19:54 -0700

    Added AuthorPermissionRowTests.

---
+++ tests/AtelierTomato.Markov.Storage.Sqlite.Test/AuthorPermissionRowTests.cs
@@ -0,0 +1,17 @@
+﻿using AtelierTomato.Markov.Model;
+using AtelierTomato.Markov.Storage.Sqlite.Model;
+using FluentAssertions;
+
+namespace AtelierTomato.Markov.Storage.Sqlite.Test
+{
+	public class AuthorPermissionRowTests
+	{
+		[Fact]
+		public void AuthorPermissionRowNullTest()
+		{
+			AuthorPermission authorPermission = new(AuthorOID.Parse("Discord:discord.com:1"), null, null);
+			AuthorPermissionRow authorPermissionRow = new AuthorPermissionRow(authorPermission);
+			authorPermissionRow.Should().BeEquivalentTo(new AuthorPermissionRow("Discord:discord.com:1", "", null));
+		}
+	}
+}

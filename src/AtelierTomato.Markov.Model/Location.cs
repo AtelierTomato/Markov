3e8ae77b0c3e8e1be8e86b5b1ee08831d8862c0b
suflors <iammagical.space@gmail.com>
2024-12-25 12:26:52 -0800

    Revert "Added GlobalAllowed to Location. (#47)"
    This reverts commit 4461e31983c0fd7dc689b8fa3184b916132a2bf2.

--- src/AtelierTomato.Markov.Model/Location.cs
+++ src/AtelierTomato.Markov.Model/Location.cs
@@ -2,6 +2,6 @@
 {
 	public record Location
 	(
-		IObjectOID ID, string Name, AuthorOID Owner, bool GlobalEnabled
+		IObjectOID ID, string Name, AuthorOID Owner
 	);
 }
--- src/AtelierTomato.Markov.Storage.Sqlite/Model/LocationRow.cs
+++ src/AtelierTomato.Markov.Storage.Sqlite/Model/LocationRow.cs
@@ -7,21 +7,18 @@ namespace AtelierTomato.Markov.Storage.Sqlite.Model
 		public string ID { get; set; }
 		public string Name { get; set; }
 		public string Owner { get; set; }
-		public int GlobalEnabled { get; set; }
-		public LocationRow(string ID, string name, string owner, int globalEnabled)
+		public LocationRow(string ID, string name, string owner)
 		{
 			this.ID = ID;
 			Name = name;
 			Owner = owner;
-			GlobalEnabled = globalEnabled;
 		}
 		public LocationRow(Location location)
 		{
 			ID = location.ID.ToString();
 			Name = location.Name;
 			Owner = location.Owner.ToString();
-			GlobalEnabled = Convert.ToInt32(location.GlobalEnabled);
 		}
-		public Location ToLocation(MultiParser<IObjectOID> objectOIDParser) => new(objectOIDParser.Parse(ID), Name, AuthorOID.Parse(Owner), Convert.ToBoolean(GlobalEnabled));
+		public Location ToLocation(MultiParser<IObjectOID> objectOIDParser) => new(objectOIDParser.Parse(ID), Name, AuthorOID.Parse(Owner));
 	}
 }

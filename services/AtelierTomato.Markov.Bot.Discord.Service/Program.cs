6cde3edddb8a6db451ee435dd4bc0fbf6a87d50d
suflors <iammagical.space@gmail.com>
2025-04-04 16:37:52 -0700

    Removed GuidTypeReader as it will no longer be necessary.

--- services/AtelierTomato.Markov.Bot.Discord.Service/Program.cs
+++ services/AtelierTomato.Markov.Bot.Discord.Service/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using AtelierTomato.Markov.Bot.Discord.Core;
 using AtelierTomato.Markov.Bot.Discord.Service;
-using AtelierTomato.Markov.Bot.Discord.Service.TypeReaders;
 using AtelierTomato.Markov.Core;
 using AtelierTomato.Markov.Core.Cooldown;
 using AtelierTomato.Markov.Core.Generation;
@@ -76,17 +75,7 @@ builder.Services
 	.AddSingleton<Cooldown>()
 	.AddSingleton<AuthorPermissionTableFormatter>()
 	.AddSingleton(_ => new MultiParser<IObjectOID>([new BookObjectOIDParser(), new SpecialObjectOIDParser(), new DiscordObjectOIDParser()]))
-	.AddSingleton(provider =>
-	{
-		var commandService = new CommandService(new CommandServiceConfig
-		{
-			DefaultRunMode = Discord.Commands.RunMode.Async
-		});
-
-		commandService.AddTypeReader<Guid>(new GuidTypeReader());
-
-		return commandService;
-	})
+	.AddSingleton(new CommandService(new CommandServiceConfig { DefaultRunMode = Discord.Commands.RunMode.Async }))
 	.AddSingleton(_ => new InteractionService(client.Rest, new InteractionServiceConfig { DefaultRunMode = Discord.Interactions.RunMode.Async }));
 
 

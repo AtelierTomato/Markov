e986341c08c0cb4e7582e5c45765aeac17875bd5
suflors <iammagical.space@gmail.com>
2025-04-06 15:57:32 -0700

    Added SetLocationGroup().

--- services/AtelierTomato.Markov.Bot.Discord.Core/CommandModules/LocationSettingsModule.cs
+++ services/AtelierTomato.Markov.Bot.Discord.Core/CommandModules/LocationSettingsModule.cs
@@ -84,5 +84,63 @@ namespace AtelierTomato.Markov.Bot.Discord.Core.CommandModules
 			await locationSettingAccess.WriteLocationSetting(locationSetting);
 			await ReplyAsync($"updated global allowed permissions for {location} to {locationSetting.GlobalAllowed}!");
 		}
+
+		[Command("setlocationgroup")]
+		[Alias("slg")]
+		[Summary("Sets the LocationGroup that will be used when generating sentences in this location.")]
+		public async Task SetLocationGroup(string locationParam, ulong? locationGroup = null)
+		{
+			var authorOID = new AuthorOID(ServiceType.Discord, options.DiscordInstance, Context.User.Id.ToString());
+			IObjectOID location = await objectOIDBuilder.Build(Context.Guild, Context.Channel, options.DiscordInstance);
+			if (!cooldown.HandleCooldown(authorOID, location, CooldownType.Default))
+			{
+				await ReplyAsync(message: "slow down!!");
+				return;
+			}
+
+			try
+			{
+				if (Enum.TryParse<DiscordLocationType>(locationParam, true, out var locationDepth))
+				{
+					if (locationDepth is DiscordLocationType.Global or DiscordLocationType.Message or DiscordLocationType.Sentence)
+					{
+						await ReplyAsync($"{nameof(LocationGroup)} settings cannot be set for {locationDepth}!");
+						return;
+					}
+					location = ((DiscordObjectOID)location).ForLocationType(locationDepth)!;
+				}
+				else
+				{
+					location = objectOIDParser.Parse(locationParam);
+				}
+			}
+			catch
+			{
+				await ReplyAsync($"location was not valid, please provide an {nameof(IObjectOID)} or a scope (such as server, channel)");
+				return;
+			}
+
+			if (!options.DeveloperIDs.Contains(Context.User.Id))
+			{
+				var owner = await locationAccess.ReadLocationOwner(location);
+				if (!(owner == authorOID))
+				{
+					await ReplyAsync("sorry, only the owner of the server is allowed to use this command!");
+					return;
+				}
+			}
+
+			var locationSetting = await locationSettingAccess.ReadLocationSetting(location) ?? new(location, [], [], [], null, null);
+			locationSetting = new(locationSetting.ID, locationSetting.WriteReactions, locationSetting.DeleteReactions, locationSetting.FailReactions, locationSetting.GlobalAllowed, locationGroup);
+			await locationSettingAccess.WriteLocationSetting(locationSetting);
+			if (locationGroup is null)
+			{
+				await ReplyAsync($"set {nameof(LocationGroup)} to default value!");
+			}
+			else
+			{
+				await ReplyAsync($"updated {nameof(LocationGroup)} for {location} to group with ID {locationGroup}!");
+			}
+		}
 	}
 }
